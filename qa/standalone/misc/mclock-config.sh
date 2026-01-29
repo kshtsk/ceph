1bf8a9465835c168471ecd933caa00084edb318d
Kyr Shatskyy <kyrylo.shatskyy@clyso.com>
2026-01-29 15:42:48 +0100

    qa/standalone/misc: debug mclock iops
    Override iops
    
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@clyso.com>

--- qa/standalone/misc/mclock-config.sh
+++ qa/standalone/misc/mclock-config.sh
@@ -205,7 +205,8 @@ function TEST_recovery_limit_adjustment_mclock() {
     run_mon $dir a || return 1
     run_mgr $dir x || return 1
 
-    run_osd $dir 0 --osd_op_queue=mclock_scheduler || return 1
+    run_osd $dir 0 --osd_op_queue=mclock_scheduler \
+                   --osd_mclock_iops_capacity_threshold_hdd=35000 || return 1
     local recoveries=$(CEPH_ARGS='' ceph --format=json daemon $(get_asok_path osd.0) \
         config get osd_recovery_max_active)
     # Get default value
